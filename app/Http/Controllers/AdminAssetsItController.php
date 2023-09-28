33fd3552db73f7f66f948ced4186c37484b0f2a9
DTCRepo <111104026+DTCRepo@users.noreply.github.com>
2023-09-28 14:36:15 +0800

    edits picking request

--- app/Http/Controllers/AdminAssetsItController.php
+++ app/Http/Controllers/AdminAssetsItController.php
@@ -470,7 +470,7 @@
 			curl_close($ch);
 
 			$cURLresponse = json_decode($cURLresponse, true);
-			dd($cURLresponse);
+			// dd($cURLresponse);
 			$data = [];
 			$count = 0;
 			if(!empty($cURLresponse["data"])) {
